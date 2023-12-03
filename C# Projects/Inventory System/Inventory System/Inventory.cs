01ab2ce7eb9c6c15ce5e5fb5cd1391ef06fe6a92
NATHANIEL\Luz Nicole <nathanielgutierrez70@gmail.com>
2023-12-03 23:41:19 +0800

    eto na ian

---
+++ C# Projects/Inventory System/Inventory System/Inventory.cs
@@ -0,0 +1,106 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_System
+{
+    public partial class frmInventory : Form
+    {
+        public frmInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Close();
+            frmLogin frmLogin = new frmLogin();
+            frmLogin.ShowDialog();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmItemAdd frmItemAdd = new frmItemAdd();
+            frmItemAdd.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmSupplierAdd frmSupplierAdd = new frmSupplierAdd();
+            frmSupplierAdd.ShowDialog();
+        }
+        private void Inventory(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            { 
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=''"))
+                    {
+                        connection.Open();
+
+                        string query = "SELECT * FROM =inventory.products";
+                        MySqlCommand command = new MySqlCommand(query, connection);
+
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            // Assuming dataGridView1 is the name of your DataGridView control
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+            private void LoadData()
+            {
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=''"))
+                    {
+                        connection.Open();
+
+                        string query = "SELECT * FROM inventory.products";
+                        MySqlCommand command = new MySqlCommand(query, connection);
+
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            // Assuming dataGridView1 is the name of your DataGridView control
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+
+                }
+            }
+
+
+
+        }
+
+    }
---
+++ C# Projects/Inventory System/Inventory System/ItemAdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_System
+{
+    public partial class frmItemAdd : Form
+    {
+        public frmItemAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+
+            this.Close();
+        }
+    }
+}
